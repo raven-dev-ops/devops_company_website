00ad57ee749e0c03a23d47934cf6f32d76138d7c
gptfleet <gptfleet@gmail.com>
2025-07-28 22:33:51 +0000

    Update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
-import { useQuizModal } from '../componenets/QuizModalContext';
+import { useQuizModal } from '../components/QuizModalContext';
 
 const servicesList = [
   { 
